8c599dd06005fcfab19c143656f91de20886df2a
Bethan Cracknell Daniels <66028934+bnc19@users.noreply.github.com>
2020-08-19 14:40:37 +0100

    Update FUN_prob_auto_trans.R

--- R/FUN_prob_auto_trans.R
+++ R/FUN_prob_auto_trans.R
@@ -297,7 +297,7 @@ plot_comp_den <- function(x, cities) {
     scale_x_continuous(limits=c(0,1),breaks=seq(0,1,0.2)) +
     ggtitle(cities) + 
     geom_segment(x = 0, xend = 5, y = 0.5, yend = 0.5, size = 0.5, linetype = 3) + 
-    geom_segment(x = 0.25, xend = 0.25, y = 0, yend = 5, size = 0.5, linetype = 2, color = "grey")
+    geom_segment(x = 0.25, xend = 0.25, y = 0, yend = 5, size = 0.5, linetype = 1, color = "grey")
 }
 
 comp_legend <- function() {
