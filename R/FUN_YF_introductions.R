a103393f61fb55705c765f8d478b7168b3662621
Bethan Cracknell Daniels <66028934+bnc19@users.noreply.github.com>
2020-10-29 21:49:58 +0000

    Update FUN_YF_introductions.R

--- R/FUN_YF_introductions.R
+++ R/FUN_YF_introductions.R
@@ -45,12 +45,12 @@ return(even_flows)
 
 ################################################################################
 # Functions to randomly draw from the incubation and infectious period
-# distributions
+# distributions, and the duration of stay
 ################################################################################
 
 incubation <- function(n) { rlnorm(n, 1.46, 0.35) }
 infectious <- function(n) {  rnorm(n, 4.5, 1.5/1.96) }
-
+stay <- function(n, mean){  rlnorm(n, mean, 2)}
 
 
 ################################################################################
@@ -81,79 +81,6 @@ countrys_at_risk <- total_importations_round[c(count), ]
 return(countrys_at_risk)
 
 }
-
-
-
-################################################################################
-# Function to select endemic countries predicted to result in at least one 
-# introduction to an Asian country 
-################################################################################
-
-select_endem <- function(x, country){
-  
-  x$endemic_country <- country  # add name of source country
-  
-  x <- rownames_to_column(x, var = "country") # convert asian country into column
-  
-  
-  over_1 <- which(x$upper_limit_95CI > 1) # select countries with at least one introduction (upper 95% CI)
-  
-  at_risk <- x[c(over_1 ), ] 
-  
-  if(dim(at_risk)[1] < 1) {  # if there are no introductions set NA
-    at_risk <-  NA } 
-  
-  if(! is.na(at_risk) ) {   # don't return NA 
-    return(at_risk)}
-}
-
-
-
-################################################################################
-#Function to plot endemic source countries
-################################################################################
-
-plot_endemic_source <- function(risk_by_ec_country) {
-  
-  country <- unique(risk_by_ec_country$country)  # list endemic countries
-
-risk_by_ec_country$endemic_country[risk_by_ec_country$endemic_country == "Democratic Republic of the Congo"]  <- "DRC"  # abbreviate for plot 
-risk_by_ec_country$endemic_country[risk_by_ec_country$endemic_country == "Ivory Coast (Cote d'Ivoire)"] <- "Ivory Coast"
-
-# filter 
-
-countries <- risk_by_ec_country %>%
-  group_by(country) %>% 
-  nest() %>% 
-  select(data) 
-
-plots <- lapply(1:6, function(i) {ggplot(countries[[2]][[i]], aes(y = mean_cases, x = endemic_country)) +
-  geom_point(size = 1) +
-  geom_errorbar(aes(ymin = lower_limit_95CI, ymax = upper_limit_95CI), width = .25) +
-  ylab(" ") + 
-  xlab(" ") + 
-  theme_hc(base_size = 16) + 
-  scale_y_continuous(limits=c(0,8), breaks=seq(0,10,1)) + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.5)) + 
-  ggtitle(country[i]) + 
-  theme(plot.title =  element_text(size = 12, face = "bold", hjust = 0.45)) })
-  
-
-grid<- plot_grid(plots[[1]], plots[[2]], plots[[3]], plots[[4]], plots[[5]], plots[[6]],  ncol = 2, align = "h")
-  
-
-
-plots_annot <- annotate_figure(grid, left = text_grob("Estimated number of introductions", rot = 90, size = 18), 
-                               bottom = text_grob("Endemic country", size = 18, vjust = -1))
-
-
-
-
-return(plots_annot)
-  
-}
-
-
 
 ################################################################################
 # Function to plot airports at risk of YF introductions
