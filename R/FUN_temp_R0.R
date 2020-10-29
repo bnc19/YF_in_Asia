7577ad4bdde235a298a0aee21f517dc1f29b2928
Bethan Cracknell Daniels <66028934+bnc19@users.noreply.github.com>
2020-10-29 21:51:03 +0000

    Update FUN_temp_R0.R

--- R/FUN_temp_R0.R
+++ R/FUN_temp_R0.R
@@ -188,227 +188,34 @@ tidy_R0 <- function(x, col_names) {
 
 
 ################################################################################
-# Function to plot R0 estimates 
+# Function to organize R0 for all temperatures
 ################################################################################
 
-plot_SEA_R0 <- function(R0_estimates, SEA) {
-  
-  x <- left_join(R0_estimates, airport_country)
-  
-  region_est <- x %>% 
-    filter(Asian_countries %in% SEA ) %>% 
-    arrange(Asian_countries, Airport)
-  
-  
-  #rename as cities 
-  
-  cities <- c("Guangzhou", "Beijing", "Shanghai", "Hong Kong", "Ahmedabad", "Mumbai", "New Delhi", "Chennai", "Hyderabad", "Tokyo", "Kuala Lumpur","Karachi", "Manila", "Singapore", "Seoul", "Bangkok") 
-  
-  
-  region_est$city <- cities
-  
-  # reorder by country by changing the factor levels 
-  
-  region_est$city <- factor(region_est$city , levels = region_est$city  [order(region_est$Asian_countries)])
-  
-  
-  rename_est <- rename(region_est, "South and East Asian Countries"  = Asian_countries)
-  
-  
-  SEA_R0_plot <- ggplot(rename_est , aes(y = mean, x = city)) +
-    geom_point(size = 2, aes(colour= `South and East Asian Countries`)) +
-    geom_errorbar(aes(ymin = lower_CI, 
-                      ymax = upper_CI, 
-                      colour=`South and East Asian Countries`), width = .25) + 
-    xlab(" ") + 
-    ylab("R0") + 
-    theme_hc(base_size = 14) + 
-    scale_color_npg() + 
-    scale_y_continuous(limits=c(0,12),breaks=seq(0,12,2))+ 
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.5), 
-          axis.title.y = element_text(vjust = 0.5, hjust=0.3, angle = 0))
-  
-  return(SEA_R0_plot)
-  
-} # plot in SEA region 
+sort_temp <- function(temperature, list_R0, airport_introductions){
 
-plot_ME_R0 <- function(R0_estimates, ME) {
-  
-  x <- left_join(R0_estimates, airport_country)
-  
-  region_est <- x %>% 
-    filter(Asian_countries %in% ME )%>% 
-    arrange(Asian_countries, Airport)
-  
-  
-  
-  #rename as cities 
-  
-cities <- c("Manama", "Amman", "Kuwait City", "Beirut", "Muscat", "Doha", "Dammam", 
-                 "Jeddah", "Medina", "Riyadh", "Istanbul", "Abu Dhabi", "Dubai")
-  
-  
-  region_est$city <- cities
-  
-  # reorder by country by changing the factor levels 
-  
-  region_est$city <- factor(region_est$city , levels = region_est$city  [order(region_est$Asian_countries)])
-  
-  
-  rename_est <- rename(region_est, "West Asia and Middle Eastern Countries"  = Asian_countries)
-  
-  
-  ME_R0_plot <- ggplot(rename_est , aes(y = mean, x = city)) +
-    geom_point(size = 2, aes(colour= `West Asia and Middle Eastern Countries`)) +
-    geom_errorbar(aes(ymin = lower_CI, 
-                      ymax = upper_CI, 
-                      colour=`West Asia and Middle Eastern Countries`), width = .25) + 
-    xlab(" ") + 
-    ylab("R0") + 
-    theme_hc(base_size = 14) + 
-    scale_color_npg() + 
-    scale_y_continuous(limits=c(0,12),breaks=seq(0,12,2))+ 
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=0.5), 
-          axis.title.y = element_text(vjust = 0.5, hjust=0.3, angle = 0))
-  
-  return(ME_R0_plot)
-  
-} # plot in ME region 
+average_temperature <- temperature[, c(1,13)]
+minimum_temperature <- temperature[, c(1,12)]
+maximum_temperature <- temperature[, c(1,11)]
 
 
+colnames(average_temperature) <- c("Airport", "temperature")
+colnames(minimum_temperature) <- c("Airport", "temperature")
+colnames(maximum_temperature) <- c("Airport", "temperature")
 
-################################################################################
-# Functions for sensitivity analysis of the effect of temperature on R0 
-################################################################################
+list_R0[[1]] <- left_join(list_R0[[1]], average_temperature)
+list_R0[[2]] <-left_join(list_R0[[2]], minimum_temperature)
+list_R0[[3]] <-left_join(list_R0[[3]], maximum_temperature)
 
-temp_param_sens <- function(Temp) {
-  
-  a <- briere(Temp, parm = c(
-    temp_params[1,1], 
-    temp_params[2,1], 
-    temp_params[3,1]))  #biting rate
-  
-  lf <- quad(Temp, parm = c(
-    temp_params[1,2], 
-    temp_params[2,2], 
-    temp_params[3,2])) # mortality 
-  
-  lf <- ifelse( lf<=0, 1, lf ) # restrict to positive 
-  
-  
-  PDR <- briere(Temp, parm = c(
-    temp_params[1,3], 
-    temp_params[2,3], 
-    temp_params[3,3])) #parasite development 
-  
-  PDR[PDR<0] = 0.001
-  EIP <- 1/PDR  #restict to positive 
-  
-  
-  
-  temp_variables <- data.frame(a, lf, EIP)
-  
-  temp_sen <- cbind(Temp, temp_variables)
-  
-  temp_sen$prop_Surv <- exp(-temp_sen$EIP / temp_sen $lf )  # include proportion surviving 
-  
-  
-  return(temp_sen)
-} # Estimate temp sensitivity parameters
+list_R0[[1]]$`Temperature (degree Celsius)` <- "Average"
+list_R0[[2]]$`Temperature (degree Celsius)` <- "Minimum"
+list_R0[[3]]$`Temperature (degree Celsius)` <- "Maximum"
+
+all_R0_values <- bind_rows(list_R0)
+city_airport <- airport_introductions[, c(1,7)]
+
+all_R0_values <- left_join(all_R0_values,city_airport)
 
 
-est_R0_HV_sen <- function(temp, n,  temp_variables) { 
-  
-  TV <- temp_variables  #temp variable can be mean, min, max 
-  
-  mosq_person <- 0.49 # select location specific mosq / person (SEA or Middle East)
-  
-  biting_rate <- TV$a[TV$temp_range == temp]  # select airport temp specific biting rate 
-  
-  
-  B_HV <- human_vec_transmission(n)  # draw from distribution of effectve human to vector transmission 
-  
-  prob_surv <- TV$prop_Surv[TV$temp_range == temp]  # select airport temp specific prob surv of EIP 
-  
-  DI <- infectious(n)  # draw from distribution for infectiousness 
-  
-  
-  R0_HV <- mosq_person * biting_rate * prob_surv * B_HV * DI  # multiply all parameters 
-  
-  return(R0_HV)
-}  # Estimate R0 HV
-
-
-est_R0_VH_sen <- function(temp, temp_variables) {
-  
-  TV <- temp_variables #temp variable can be mean, min, max 
-  
-  biting_rate <- TV$a[TV$temp_range == temp]# select location specific mosq / person (SEA or Middle East)
-  
-  
-  B_VH <- 0.25  # effective transmission from vector to human set at 0.25 following "Global Risk and Elimination of Yellow Fever Epidemics"
-  
-
-  
-  lf <- TV$lf[TV$temp_range == temp]  # select airport temp specific longevity 
-  
-  
-  R0 <- biting_rate * lf* B_VH  # multiply all parameters 
-  
-  return(R0)
-} # Estimate R0 VH 
-
-
-est_R0_sen <- function(temp, nsim, temp_variables) {
-  
-  TV <- temp_variables
-  
-  temp <- temp
-  
-  n <- nsim
-  
-  
-  R0_VH <- est_R0_VH_sen(temp, TV)  # estimate R0_VH
-  
-  R0_HV <- est_R0_HV_sen(temp, n, TV) # estimate R0_HV
-  
-  
-  R0 <- R0_VH * R0_HV  # multiply together 
-  
-  
-  mean <- mean(R0, na.rm = TRUE)   # calculate mean, 95 CI, quantile 
-  quant <- quantile(R0, c(.025, .975), na.rm = TRUE)
- 
-  out <- data.frame( mean = mean, 
-                     lower_CI = quant[1],
-                     upper_CI = quant[2])
-  #return data frame of variables 
-  
-  return(out)
-  
-  
-} # Estimate R0
-
-
-tidy_data <- function(x, name_col) { #tidy data
-  
-  x <- all_R0_estimates_sens
-  name_col <- temp_range
-  
-  x [is.na(x )] <- 0
-  write.csv(x, file = "x")
-  read <- column_to_rownames(read.csv("x"), "X")
-
-  colnames(read) <- name_col #name columns temperature   
-  df <- as.data.frame(t(read))  # transpose 
-  
-  output <- df %>%  
-    rownames_to_column(var = "Variable")
-  
-
-  
-  return(output)
-  
-} # tidy output of data 
+return(all_R0_values)}
 
 
